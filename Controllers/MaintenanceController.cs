58a7c1e27b69b8c4e4ba0a8496e0edc30611ff5f
Edilma Riano <edilmaria@hotmail.com>
2020-02-20 15:20:28 -0500

    worked on maintenance controller, viewModel and views

--- Controllers/MaintenanceController.cs
+++ Controllers/MaintenanceController.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using AutoTracker.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using AutoTracker.Models;
+using AutoTracker.ViewModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace AutoTracker.Controllers
 {
@@ -16,20 +20,35 @@ namespace AutoTracker.Controllers
             context = dbContext;
         }
 
-
+        //Here we show all the maintenance performed in the car
         public IActionResult Index()
         {
-            return View();
+            IList<Maintenance> AllMaintenances = context.Maintenances.ToList();
+            return View(AllMaintenances);
         }
         //Get where the user lands and sees the button
         public IActionResult Add()
         {
-            return View();
+            AddMaintenanceViewModel addMaintenanceViewModel = new AddMaintenanceViewModel();
+            return View(addMaintenanceViewModel);
 
         }
         [HttpPost]
         public IActionResult Add(AddMaintenanceViewModel addMaintenanceViewModel)
         {
+            if (ModelState.IsValid)
+            {
+                Maintenance newMaintenance = new Maintenance()
+                {
+                    MaintenacePerformedMiles = addMaintenanceViewModel.MaintenacePerformedMiles,
+                    MaintenancePerformedDate = addMaintenanceViewModel.MaintenancePerformedDate,
+                    MaintenanceCost = addMaintenanceViewModel.MaintenanceCost,
+                    MaintenanceTypeID = addMaintenanceViewModel.MaintenanceTypeID
+                };
+                context.Maintenances.Add(newMaintenance);
+                context.SaveChanges();
+                return Redirect("/Maintenance/Index/" + newMaintenance.ID);
+            };
             return View();
 
         }
---
+++ ViewModel/AddMaintenanceViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations;
+using AutoTracker.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AutoTracker.ViewModels
+{
+    public class AddMaintenanceViewModel
+    {
+        [Required]
+        [Display(Name = "Maintenance Service")]
+        //public string SelectMaintenace { get; set; }
+        
+        public int MaintenanceTypeID { get; set; }
+        public IEnumerable<SelectListItem> maintenanceTypes { get; set; }
+        
+        [Required (ErrorMessage = "Please enter a valid number of miles")]
+        [Display(Name = "Miles in Odometer")]
+        public int MaintenacePerformedMiles { get; set; }
+        
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Date of Maintenance")]
+        public DateTime MaintenancePerformedDate { get; set; }
+        
+        [Display(Name = "Maintenance Cost")]
+        [Range(0, 9999.99)]
+        public double MaintenanceCost { get; set; }
+
+
+        public AddMaintenanceViewModel()
+        { }
+
+        public AddMaintenanceViewModel(IEnumerable<MaintenanceType> maintenanceTypes)
+        {
+           //Car = car;
+            List<SelectListItem> MaintenanceTypes = new List<SelectListItem>();
+            
+            foreach (MaintenanceType maintenanceType in maintenanceTypes)
+            {
+                MaintenanceTypes.Add(new SelectListItem {
+                    Value = maintenanceType.MaintenanceTypeID.ToString(),
+                        Text = maintenanceType.Description
+                    });
+            }
+        }
+
+    }
+}
