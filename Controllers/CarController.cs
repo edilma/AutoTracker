b9b8d474878514cc28c4e229909b1b7c6b0a5aa7
Ardley Pierre <ardleypierre@outlook.com>
2020-02-23 22:15:06 -0500

    Sprint to to the finish line on a Sunday night

--- Controllers/CarController.cs
+++ Controllers/CarController.cs
@@ -2,10 +2,52 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoTracker.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using AutoTracker.Models;
+using AutoTracker.ViewModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace AutoTracker.Controllers
 {
-    public class CarController
+    public class CarController : Controller
     {
+
+        private AutoTrackerDbContext context;
+        public CarController(AutoTrackerDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            IList<Car> AllCars = context.Cars.ToList();
+            return View(AllCars);
+        }
+   
+        public IActionResult Add(int id)
+        {
+
+        }
+        [HttpPost]
+        public IActionResult Add(AddCarViewModel addCarViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+
+                
+                Car newCar = new Car()
+                {
+                    
+
+                };
+                context.Cars.Add(newCar);
+                context.SaveChanges();
+                return Redirect("/Home/MainPage");
+            };
+            return View();
+
+        }
     }
 }
--- ViewModel/AddCarViewModel.cs
+++ ViewModel/AddCarViewModel.cs
@@ -1,11 +1,67 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations;
+using AutoTracker.Models;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
-namespace AutoTracker.ViewModel
+namespace AutoTracker.ViewModels
 {
     public class AddCarViewModel
     {
+        public int CarID { get; set; }
+        [Required]
+        [Display(Name = "Your Car")]
+
+
+        public int CarCategoryID { get; set; }
+        public List<SelectListItem> CarTypes { get; set; }
+
+        [Required(ErrorMessage = "Please enter a valid Vin")]
+        [Display(Name = "Vin")]
+        public string VinNumber { get; set; }
+
+        [Required(ErrorMessage = "Please enter a valid make")]
+        [Display(Name = "Make")]
+        public string Make { get; set; }
+
+        [Required(ErrorMessage = "Please enter a valid model")]
+        [Display(Name = "Model")]
+        public string Model { get; set; }
+
+        [Required(ErrorMessage = "Please enter a valid year")]
+        [Display(Name = "Year")]
+        public int Year { get; set; }
+
+        [Required(ErrorMessage = "Please enter the current miles")]
+        [Display(Name = "CurrentMiles")]
+        public int CurrentMiles { get; set; }
+
+        [Required(ErrorMessage = "Please enter the next maintenance miles")]
+        [Display(Name = "NextMaintenanceMiles")]
+        public int NextMaintenanceMiles { get; set; }
+
+        [Required(ErrorMessage = "Please enter next maintenace day")]
+        [Display(Name = "Next Maintenance day")]
+        public int NextMaintenanceDays { get; set; }
+
+        public AddCarViewModel()
+        { }
+
+        public AddCarViewModel(IEnumerable<MaintenanceType> CarCategories)
+        {
+            //Car = car;
+            CarCategories = new List<SelectListItem>();
+
+            foreach (CarCategory Category in CarCategories)
+            {
+                CarCategories.Add(new SelectListItem
+                {
+                    Value = Category.ID.ToString(),
+                    Text = Category.Make
+                });
+            }
+        }
+
     }
 }
